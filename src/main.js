cb544e0b25be45a448b408512e73488a9b541a60
CarlosOliveiragithub <github.carlosoliveira@gmail.com>
2022-12-01 23:13:53 +0000

    Segunda comentario

--- src/main.js
+++ src/main.js
@@ -14,5 +14,5 @@ var resultado;
 for(let conta =1; conta <= 10; conta++){
 
 resultado =  conta * 2 ;
-    console.log('Tabuada do 2  = ' + conta + ' * 2 =' + resultado )
+    console.log('Tabuada do 2 = ' + conta + ' * 2 =' + resultado )
 }
