fdd697851b889a019b28516fbb4f8b4e57449271
CarlosOliveiragithub <github.carlosoliveira@gmail.com>
2022-12-01 23:12:28 +0000

    Primeiro comentario

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// const leiaEntrada = require("readline");
+
+// const lendo = leiaEntrada.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// lendo.question("Qual seu nome? ", function (answer) {
+//   console.log(`Oh, seu nome é ${answer}`);
+// });
+
+var resultado;
+
+for(let conta =1; conta <= 10; conta++){
+
+resultado =  conta * 2 ;
+    console.log('Tabuada do 2  = ' + conta + ' * 2 =' + resultado )
+}
