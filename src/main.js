3692d6bd1c0fc66a7281b603c3750ed983b0bc05
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2023-05-02 20:30:34 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//--QUESTIONARIO---------------------------
 // const leiaEntrada = require("readline");
 
 // const lendo = leiaEntrada.createInterface({
@@ -9,6 +10,22 @@
 //   console.log(`Oh, seu nome é ${answer}`);
 // });
 
+
+// MEDIA DE 2 VALORES
+// const prompt = require('prompt-sync')
+// const prompt1 = prompt()
+
+// var resultado;
+
+// var n1 = parseInt(prompt1("Digite o primeiro valor: "));
+
+// var n2 = parseInt(prompt1("Digite o segundo valor: "));
+
+// resultado = (n1 + n2) / 2; 
+
+// console.log("A média é igual a " + resultado);
+
+//TABUADA----------------------------------------------
 var resultado;
 
 for(let conta =1; conta <= 10; conta++){
